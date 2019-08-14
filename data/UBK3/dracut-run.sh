9f73390e1514d0a8a52152aed34a645db9bcfce7
rezso <rezso@rezso.net>
2019-08-14 16:08:54 +0200

    felesleges script törlése, sources.list javítása

--- data/UBK3/dracut-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eu
-
-KERN=$(readlink /boot/kernel.x86_64)
-KERN=${KERN/kernel-/}
-KVER=$(echo $KERN | sed -e "s/\.x86_64//")
-
-dracut -f -N -c /etc/dracut.conf.d/uhu-live.conf /boot/initrd-$KVER.x86_64.img
